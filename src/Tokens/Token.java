17897f38300afd1db7f410ae8d01625ee4bda33e
Jose Adriel Madrigal Gonzalez <joseadriel93@gmail.com>
2015-03-30 18:42:49 -0700

    Token class
    Es una clase que va a contener todo lo relacionado a un token , y le
    agregue el metodo para extraer a todos los tokens que hay, ahorita solo
    extrae los tokens que son de mas de dos caracteres

--- src/Tokens/Token.java
+++ src/Tokens/Token.java
@@ -2,13 +2,36 @@ package Tokens;
 
 import java.util.*;
 
-public class Token {
-
+public class Token implements ValidTokens{
+	
 	List<Token> tokens;
-	private List<Token> tokenize(String source){
+	public String text;
+	public TokenType type;
+	
+	public Token( String text, TokenType type ){
+		this.text = text;
+		this.type = type;
+	}
+	
+	private List<Token> tokenize(String[] source){
 		
 		tokens = new ArrayList<Token>();
-		
+		boolean found;
+		String str;
+		for( int i = 0; i < source.length; i++ ){
+			found = false;
+			for( int j = 0; j < stringTokens.length; j++ ){
+				if( source[i].equals( stringTokens[j] ) ){
+					tokens.add( new Token( stringTokens[j], tokenType[0][i] ));
+					found = true;
+					break;
+				}
+			}
+			if(found == false){
+				str = source[i];
+				
+			}
+		}
 		
 		
 		return tokens;
