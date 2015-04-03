6ba354f715c838d5c52a6272c978b4210f053cb7
Jose Adriel Madrigal Gonzalez <joseadriel93@gmail.com>
2015-04-03 09:10:47 -0700

    Arregle el erro y habilite comentarios

--- src/Main/Lexer.java
+++ src/Main/Lexer.java
@@ -9,6 +9,8 @@ import Tokens.Token;
 public class Lexer{
 
 	private String[] source;		
+	private String[][] src;
+	private ReadFile file;
 	ArrayList<Token> tokenList = new ArrayList();
 	Token token = new Token(null,null);
 	
@@ -17,8 +19,14 @@ public class Lexer{
 		this.source = source;
 	}
 	
+	public Lexer(String[][] src){
+		this.src = src;
+	}
+	
 	public void initLexer(){		
-		tokenList=(ArrayList<Token>) token.tokenize(source);		
+//		tokenList=(ArrayList<Token>) token.tokenize(source);
+		String[] contentLines = getContentTextFile().split("\n");
+		tokenList=(ArrayList<Token>) token.tokenize(src,contentLines);		
 		showTokens();
 	}
 
@@ -26,6 +34,14 @@ public class Lexer{
 		for(int i=0; i<tokenList.size(); i++){
 			System.out.println(i+"	Token: "+tokenList.get(i).getText()+ "		Tipo: "+tokenList.get(i).getToken());
 		}
+	}
+	
+	public String getContentTextFile(){
+		return file.getContentTextFile();
+	}
+	
+	public void fileUsed(ReadFile file){
+		this.file = file;
 	}
 }
  
